0527bc1adfb66b055e2a594c7e649b9d3f6826c7
bryan <bryanvivanco97@outlook.com>
2025-06-08 15:02:53 -0500

    fix: button svg size

--- src/components/atoms/GameButton.jsx
+++ src/components/atoms/GameButton.jsx
@@ -4,7 +4,7 @@ import sprite from '/assets/sprite.svg'
 const GameButton = ({ico, to, gameName}) => {
     return <Link to={to}>
         <li className="bg-sky-500 rounded-2xl flex flex-col justify-center items-center group w-40 active:animate-jelly">
-            <svg className='group hover:animate-rotate-360 w-12 h-12' >
+            <svg className='group hover:animate-rotate-360 min-w-18 min-h-18' >
                 <use href={`${sprite}#${ico}`}/>
             </svg>
             <h1 className='font-Inter-Variable font-bold mb-2 text-xl'>{gameName}</h1>
