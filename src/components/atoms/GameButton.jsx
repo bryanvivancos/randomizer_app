69de0bc06a1558da4851affef9bb69421530bd6a
bryan <bryanvivanco97@outlook.com>
2025-06-08 14:58:09 -0500

    update: change button for li

--- src/components/atoms/GameButton.jsx
+++ src/components/atoms/GameButton.jsx
@@ -3,12 +3,12 @@ import sprite from '/assets/sprite.svg'
 
 const GameButton = ({ico, to, gameName}) => {
     return <Link to={to}>
-        <button className="bg-sky-500 rounded-2xl flex flex-col justify-center items-center group w-40 active:animate-rubber-band">
-            <svg className='group hover:animate-rotate-360 w-full'>
+        <li className="bg-sky-500 rounded-2xl flex flex-col justify-center items-center group w-40 active:animate-jelly">
+            <svg className='group hover:animate-rotate-360 w-12 h-12' >
                 <use href={`${sprite}#${ico}`}/>
             </svg>
             <h1 className='font-Inter-Variable font-bold mb-2 text-xl'>{gameName}</h1>
-        </button>
+        </li>
     </Link>
 }
 
